e0363ab85a7f8339b13fb03f15c00c13f24fbde4
ghost-fk <fayyad_r22@yahoo.com>
2018-09-07 16:10:02 +0200

    import fontawesome and use star icon

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
 
 
 
 const Stars = () =>{
   return  (
     <div>
-      ...
+      <FontAwesomeIcon  icon={faStar}/>
     </div>
   )
 }
