0bd43ce230640ed443eae44a95c0950249a1affb
ghost-fk <fayyad_r22@yahoo.com>
2018-09-07 16:03:45 +0200

    add views components and have two components button and answer and import in game component and test

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import Stars from './Stars'
+import Button from '../views/Button'
+import Answer from '../views/Answer'
 
 class Game extends Component{
   render(){
-    return(<h1>Play Nine</h1>)
+    return(
+      <Fragment>
+      <h1>Play Nine</h1>
+    <Stars />
+    <Button />
+    <Answer />
+  </Fragment>
+)
   }
 }
 
---
+++ src/views/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Button = ( ) =>{
+  return (<p>test Button</p>)
+}
+
+export default Button
